db673e2c3960ece121c37391a9f1443dc7e9deff
DoHyeon <wjs505@pi-lab.io>
2023-12-06 16:54:17 +0900

    update: conventions

--- contracts/implementation/StorageContract.sol
+++ contracts/implementation/StorageContract.sol
@@ -13,11 +13,13 @@ abstract contract StorageContract {
     Token
   }
 
-  mapping(string => TokenState) state;
-  mapping(address => bool) whiteList;
-  ISBTContract tokenContract;
-  IERC20 public constant usdcContract = IERC20(0x28661511CDA7119B2185c647F23106a637CC074f);
-  ISBTPriceContract priceContract;
   uint256 count;
   bool public killSwitch;
+
+  mapping(string => TokenState) state;
+  mapping(address => bool) whiteList;
+
+  ISBTContract tokenContract;
+  ISBTPriceContract priceContract;
+  IERC20 public constant usdcContract = IERC20(0x28661511CDA7119B2185c647F23106a637CC074f);
 }
